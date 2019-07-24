557e019a59151a75c342d50d51d878c40148aeb5
Zhengpeng Li <zhli@FetchAdmins-MacBook-Pro.local>
2019-07-24 11:33:40 -0700

    redux for chart and compare map

---
+++ src/actions/chart/actions.ts
@@ -0,0 +1,47 @@
+import { ChartPoint } from '../../definitions'
+
+import {
+    SET_CHART ,
+    START_CHART_LOADING,
+    STOP_CHART_LOADING,
+    FAIL_CHART_LOADING,
+    CHANGE_STARTDATE,
+    CHANGE_ENDDATE,
+    CHANGE_CUR_SELECTION,
+    CHANGE_INTERVAL,
+    chartAction,
+    chartStoreState
+} from '.'
+
+import { AppState } from '../../store'
+
+import { ThunkDispatch } from 'redux-thunk'
+import axios, { AxiosResponse } from 'axios'
+
+//TODO: what exactly does history chart do ?
+export const loadChart = () => async (dispatch: ThunkDispatch<AppState, undefined, chartAction>) => {
+    dispatch({ type: START_CHART_LOADING })
+
+    try {
+        const res: AxiosResponse = await axios.get('api/rooms', {
+            params: {
+                // timeFrame: ,
+                // startTime:,
+                // endTime: ,
+            }
+        })
+
+        let chartPoints = res.data.map((point: any): ChartPoint => ({
+            date: point.date,
+            value: point.value,
+        }))
+
+        dispatch({ type: SET_CHART, chartPoints })
+
+    } catch(err) {
+        dispatch({ type: FAIL_CHART_LOADING, message: 'Failed to fetch rooms: ' + err })
+    }
+
+
+    dispatch({ type: STOP_CHART_LOADING })
+}
---
+++ src/actions/cmpMap/actions.ts
@@ -0,0 +1,67 @@
+import { MLPoint } from '../../definitions'
+import {
+    LOAD_HEATMAP1,
+    LOAD_HEATMAP2,
+    CLEAR_HEATMAP1,
+    CLEAR_HEATMAP2,
+    // START_ML1_LOADIND,
+    // START_ML2_LOADIND,
+    // FAIL_ML1_LOADING,
+    // FAIL_ML2_LOADING,
+    // STOP_ML1_LOADING,
+    // STOP_ML2_LOADING,
+    CHANGE_INTERVAL,
+    CHANGE_DATE1,
+    CHANGE_DATE2,
+    CmpMapAction
+}from '.'
+
+// Suppose we are using websocket to handle mlpoints data
+// how to between mlPoints1 and mlPoints2
+export const loadHeatmap1 = (mlPoints1: MLPoint[]): CmpMapAction => ({
+    type: LOAD_HEATMAP1,
+    mlPoints1
+})
+export const loadHeatmap2 = (mlPoints2: MLPoint[]): CmpMapAction => ({
+    type: LOAD_HEATMAP2,
+    mlPoints2
+})
+export const clearHeatmap1 = (): CmpMapAction => ({
+    type: CLEAR_HEATMAP1
+})
+export const clearHeatmap2 = (): CmpMapAction => ({
+    type: CLEAR_HEATMAP2
+})
+// export const startMl1Loading = (): CmpMapAction => ({
+//     type: START_ML1_LOADIND
+// })
+// export const startMl2Loading = (): CmpMapAction => ({
+//     type: START_ML2_LOADIND
+// })
+// export const failMl1Loading = (message: string): CmpMapAction => ({
+//     type: FAIL_ML1_LOADING,
+//     message
+// })
+// export const failMl2Loading = (message: string): CmpMapAction => ({
+//     type: FAIL_ML2_LOADING,
+//     message
+// })
+// export const stopMl1Loading = () => ({
+//     type: STOP_ML1_LOADING
+// })
+// export const stopMl2Loading = () => ({
+//     type: STOP_ML2_LOADING
+// })
+export const changeInterval = (interval:string) => ({
+    type: CHANGE_INTERVAL,
+    interval
+})
+export const changeDate1 = (date1: Date) => ({
+    type: CHANGE_DATE1,
+    date1
+})
+export const changeDate2 = (date2: Date) => ({
+    type: CHANGE_DATE2,
+    date2
+})
+
